a9bd4b40be4d130c9c9db3c17434cc6bfd4aef2d
IanMedeirosMartins <ianmmartins2017@gmail.com>
2026-03-03 00:02:55 +0000

    aula 2 react

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header />
         <img src="Octocat.png" className="App-logo" alt="logo" />
        <h1>Hello World!!</h1>
         
---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+        <header> 
+            <h1> Header Componet</h1>
+        </header> 
+);
+}
+
+export default Header;
